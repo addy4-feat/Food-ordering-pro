9275a698244dbaea84a8082ee2ae10061ea9bbd3
Aditi Panwar <101512552+addy4-feat@users.noreply.github.com>
2022-05-27 20:35:25 +0530

    Delete Food_Ordering_Project/Food_Ordering_Project directory

--- Food_Ordering_Project/Food_Ordering_Project/src/org/birlasoft/food/dao/FoodDao.java
+++
@@ -1,9 +0,0 @@
-package org.birlasoft.food.dao;
-import java.sql.SQLException;
-
-import org.birlasoft.food.inventory.Mainclass;
-
-public interface FoodDao {
-	public void menu() throws SQLException;
-	public boolean order(int choose) throws SQLException;
-}
